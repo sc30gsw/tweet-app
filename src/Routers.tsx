51db59d3a4a66dde25d0fccd09a6e6937410bf81
sc30gsw <haidoushi875@gmail.com>
2022-12-29 20:06:01 +0900

    ログインコンポーネントのルーティングの追加

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./components/auth/Auth";
+import AuthLogin from "./components/auth/AuthLogin";
 import Home from "./components/Home";
 
 const Routers = () => {
@@ -8,6 +9,7 @@ const Routers = () => {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/signup" element={<Auth />} />
+				<Route path="/login" element={<AuthLogin />} />
 				<Route path="/" element={<Home />} />
 			</Routes>
 		</BrowserRouter>

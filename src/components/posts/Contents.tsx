7047f8eb03b931b9eff67e2ce98736a978e6bef0
sc30gsw <haidoushi875@gmail.com>
2022-12-30 16:01:25 +0900

    firebaseから投稿を取得し、Itemコンポーネントに切り出し

--- src/components/posts/Contents.tsx
+++ src/components/posts/Contents.tsx
@@ -1,8 +1,9 @@
-import { ArrowDropDown } from "@mui/icons-material";
-import React from "react";
-import { Link } from "react-router-dom";
+import { collection, DocumentData, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { useAuthContext } from "../../context/AuthProvider";
+import { db } from "../../firebase/firebase";
+
+import Item from "./Item";
 
 const StyledContents = styled.div`
 	width: 100%;
@@ -13,124 +14,19 @@ const StyledContents = styled.div`
 	max-width: 660px;
 `;
 
-const StyledContentPost = styled.div`
-	margin: 20px 0;
-	padding: 30px;
-	position: relative;
-	height: 400px;
-	color: #fff;
-	background-size: cover;
-	background-position: center center;
-	border-radius: 3px;
-	box-shadow: 0 0 10px rgb(41 41 41 / 20%);
-	background-image: url("images/img1.jpeg");
-	z-index: 1;
-	border-radius: 3px;
-`;
-
-const StyledMore = styled.div`
-	cursor: pointer;
-	top: 20px;
-	right: 30px;
-	z-index: 2;
-	height: 100px;
-	width: 80px;
-	text-align: right;
-	position: absolute;
-	:hover {
-		ul {
-			display: block;
-		}
-	}
-`;
-
-const StyledMoreUL = styled.ul`
-	position: absolute;
-	text-align: left;
-	width: 80px;
-	right: 0;
-	font-size: 12px;
-	background-color: #fff;
-	border: 1px solid #ddd;
-	box-shadow: 1px 1px 2px rgb(0 0 0 / 20%);
-	border-radius: 3px;
-	display: none;
-	:hover {
-		background-color: #57c3e9;
-		a {
-			color: #fff;
-		}
-	}
-`;
-
-const StyledMoreLI = styled.li`
-	padding: 5px;
-	display: block;
-	a {
-		color: gray;
-		text-decoration: none;
-	}
-`;
-
-const StyledPostText = styled.p`
-	position: absolute;
-	left: 30px;
-	right: 100px;
-	bottom: 30px;
-	margin: 0;
-	font-size: 20px;
-	z-index: 2;
-	font-weight: bold;
-	text-shadow: 0 0 2px #000, 0 0 1px #000, 0 0 1px #000;
-`;
-
-const StyledPostUsername = styled.span`
-	position: absolute;
-	right: 30px;
-	bottom: 30px;
-	z-index: 2;
-	font-size: 13px;
-	a {
-		text-decoration: none;
-		display: block;
-		color: #aaa;
-		:hover {
-			text-decoration: underline;
-		}
-	}
-	span {
-		font-size: 9px;
-		display: block;
-	}
-`;
-
 const Contents = () => {
-	const user = useAuthContext();
+	const [posts, setPosts] = useState<DocumentData[]>([]);
+	useEffect(() => {
+		const postData = collection(db, "posts");
+		getDocs(postData).then((querySnapshot) => {
+			setPosts(querySnapshot.docs.map((doc) => doc.data()));
+		});
+	}, []);
 	return (
 		<StyledContents>
-			<StyledContentPost>
-				<StyledMore>
-					<ArrowDropDown
-						style={{
-							position: "relative",
-							color: "gray",
-							fontSize: "35px",
-						}}
-					/>
-					<StyledMoreUL>
-						<StyledMoreLI>
-							<Link to="#">詳細</Link>
-						</StyledMoreLI>
-					</StyledMoreUL>
-				</StyledMore>
-				<StyledPostText>投稿</StyledPostText>
-				<StyledPostUsername>
-					<Link to="#">
-						<span>投稿者</span>
-						{user.currentUser?.displayName}
-					</Link>
-				</StyledPostUsername>
-			</StyledContentPost>
+			{posts.map((post) => (
+				<Item key={post.id} post={post} />
+			))}
 		</StyledContents>
 	);
 };
---
+++ src/components/posts/Item.tsx
@@ -0,0 +1,153 @@
+import { ArrowDropDown } from "@mui/icons-material";
+import { DocumentData } from "firebase/firestore";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useAuthContext } from "../../context/AuthProvider";
+
+const StyledContentPost = styled.div`
+	margin: 20px 0;
+	padding: 30px;
+	position: relative;
+	height: 400px;
+	color: #fff;
+	background-size: cover;
+	background-position: center center;
+	border-radius: 3px;
+	box-shadow: 0 0 10px rgb(41 41 41 / 20%);
+	z-index: 1;
+	border-radius: 3px;
+`;
+
+const StyledMore = styled.div`
+	cursor: pointer;
+	top: 20px;
+	right: 30px;
+	z-index: 2;
+	height: 100px;
+	width: 80px;
+	text-align: right;
+	position: absolute;
+	:hover {
+		ul {
+			display: block;
+		}
+	}
+`;
+
+const StyledMoreUL = styled.ul`
+	position: absolute;
+	text-align: left;
+	width: 80px;
+	right: 0;
+	font-size: 12px;
+	background-color: #fff;
+	border: 1px solid #ddd;
+	box-shadow: 1px 1px 2px rgb(0 0 0 / 20%);
+	border-radius: 3px;
+	display: none;
+`;
+
+const StyledMoreLI = styled.li`
+	padding: 5px;
+	display: block;
+	a {
+		color: gray;
+		text-decoration: none;
+	}
+	:hover {
+		background-color: #57c3e9;
+		a {
+			color: #fff;
+		}
+	}
+`;
+
+const StyledPostText = styled.p`
+	position: absolute;
+	left: 30px;
+	right: 100px;
+	bottom: 30px;
+	margin: 0;
+	font-size: 20px;
+	z-index: 2;
+	font-weight: bold;
+	text-shadow: 0 0 2px #000, 0 0 1px #000, 0 0 1px #000;
+`;
+
+const StyledPostUsername = styled.span`
+	position: absolute;
+	right: 30px;
+	bottom: 30px;
+	z-index: 2;
+	font-size: 13px;
+	a {
+		text-decoration: none;
+		display: block;
+		color: #aaa;
+		:hover {
+			text-decoration: underline;
+		}
+	}
+	span {
+		font-size: 9px;
+		display: block;
+	}
+`;
+
+type Props = {
+	post: DocumentData;
+};
+
+const Item = ({ post }: Props) => {
+	const user = useAuthContext().currentUser;
+	const currentUserId = user?.uid;
+	return (
+		<>
+			<StyledContentPost
+				key={post.id}
+				style={{ backgroundImage: `url(${post.image})` }}
+			>
+				<StyledMore>
+					<ArrowDropDown
+						style={{
+							position: "relative",
+							color: "gray",
+							fontSize: "35px",
+						}}
+					/>
+					{post.userId === currentUserId ? (
+						<StyledMoreUL>
+							<StyledMoreLI>
+								<Link to="#">詳細</Link>
+							</StyledMoreLI>
+							<StyledMoreLI>
+								<Link to="#">編集</Link>
+							</StyledMoreLI>
+							<StyledMoreLI>
+								<Link to="#">削除</Link>
+							</StyledMoreLI>
+						</StyledMoreUL>
+					) : (
+						<StyledMoreUL>
+							<StyledMoreLI>
+								<Link to="#">詳細</Link>
+							</StyledMoreLI>
+						</StyledMoreUL>
+					)}
+				</StyledMore>
+				<StyledPostText>{post.text}</StyledPostText>
+				<StyledPostUsername>
+					<Link
+						to={`/users/${post.userId}`}
+						state={{ username: post.username, userId: post.userId }}
+					>
+						<span>投稿者</span>
+						{post.username}
+					</Link>
+				</StyledPostUsername>
+			</StyledContentPost>
+		</>
+	);
+};
+
+export default Item;

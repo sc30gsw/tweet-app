250814835662ca45531ab58f3410617ac9d8a55d
sc30gsw <haidoushi875@gmail.com>
2022-12-30 16:01:42 +0900

    マイページのコンポーネント作成

---
+++ src/components/user/Mypage.tsx
@@ -0,0 +1,62 @@
+import {
+	collection,
+	DocumentData,
+	getDocs,
+	query,
+	where,
+} from "firebase/firestore";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { db } from "../../firebase/firebase";
+import useAuthState from "../../lib/AuthState";
+import Header from "../Header";
+import Item from "../posts/Item";
+
+const StyledText = styled.p`
+	max-width: 660px;
+	width: 100%;
+	margin-left: auto;
+	margin-right: auto;
+`;
+
+const StyledContents = styled.div`
+	width: 100%;
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: 0;
+	margin-bottom: 0;
+	max-width: 660px;
+`;
+
+type State = {
+	username: string;
+	userId: string;
+};
+
+const Mypage = () => {
+	useAuthState();
+	const location = useLocation();
+	const { username, userId } = location.state as State;
+	const [posts, setPosts] = useState<DocumentData[]>([]);
+
+	const postData = collection(db, "posts");
+	const userPostData = query(postData, where("userId", "==", userId));
+	getDocs(userPostData).then((querySnapshot) => {
+		setPosts(querySnapshot.docs.map((doc) => doc.data()));
+	});
+
+	return (
+		<>
+			<Header />
+			<StyledText>{username}さんの投稿一覧</StyledText>
+			<StyledContents>
+				{posts.map((post) => (
+					<Item key={post.id} post={post} />
+				))}
+			</StyledContents>
+		</>
+	);
+};
+
+export default Mypage;
